255949fac94e922414c070ad8dc95eab67148afd
tejasri3123 <nallatejasri@klh.edu.in>
2026-08-11 10:35:49 +0530

    Add files via upload

---
+++ 2520030132_practical/hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello Linux\n");
+    return 0;
+}
+
+Compile: gcc hello.c -o hello
+
+./hello
+
+gcc hello.c -o
+sudo apt install gcc
+
---
+++ 2520030132_practical/name.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int main()
+{
+    char name[30];
+
+    printf("Enter your name: ");
+    scanf("%s",name);
+
+    printf("Welcome %s\n",name);
+
+    return 0;
+}
